dc68dcb7c67d8cd4de2bf264b31e232653abd279
Kathya Pravia <kathya.pravia@gmail.com>
2017-06-22 18:49:08 -0500

    Semana 3
    Aplicacion  Web Semana 3

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  post_id    :integer
+#  bodey      :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#
+
 class Comment < ApplicationRecord
   belongs_to :post
+  validates_presence_of :post_id
+  validates_presence_of :bodey
+#  belongs_to :user
 end
